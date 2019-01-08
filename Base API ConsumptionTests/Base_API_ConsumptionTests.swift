e80e8604792a4187eadb890922ddc079b65a3f8e
Abel Martínez <ricardo.martinez@globant.com>
2019-01-07 20:38:21 -0600

    Initial Commit

---
+++ Base API ConsumptionTests/Base_API_ConsumptionTests.swift
@@ -0,0 +1,34 @@
+//
+//  Base_API_ConsumptionTests.swift
+//  Base API ConsumptionTests
+//
+//  Created by Ricardo Abel Martinez Vivanco on 1/7/19.
+//  Copyright © 2019 personal. All rights reserved.
+//
+
+import XCTest
+@testable import Base_API_Consumption
+
+class Base_API_ConsumptionTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
